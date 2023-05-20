1989a23bb3efbd5785bff54728ef0c0177155822
frank <frank@FrancoRodriguez>
2023-05-20 20:11:14 -0300

    Se crea ListaNuevoProducto y nuevo_productoController

---
+++ TP2_Grupo21/src/main/java/ar/edu/unju/fi/listas/ListaNuevoProducto.java
@@ -0,0 +1,23 @@
+package ar.edu.unju.fi.listas;
+import java.util.ArrayList;
+import java.util.List;
+import ar.edu.unju.fi.model.Producto;
+
+public class ListaNuevoProducto {
+
+	private List<Producto> Productos;
+	
+	public ListaNuevoProducto() {
+		Productos= new ArrayList<Producto>();
+		Productos.add(new Producto("Correa", "Vestimenta", 30, 1234, 2000));
+		Productos.add(new Producto("Cucha", "Cuidado", 30, 4321, 2500));
+		Productos.add(new Producto("Pelota", "Juguete", 30, 2345, 1500));
+	}
+	public List<Producto> getProductos(){
+		return Productos;
+	}
+	
+	public void setproducto(List<Producto> producto) {
+		this.Productos = producto;
+	}
+}
