70564758a431f12173064dc8533475f6bfa2eefa
lalex <lalex@DESKTOP-44R5U4S>
2023-06-24 18:28:37 -0300

    Interfaz repository de empleado

---
+++ TP2_Grupo21/src/main/java/ar/edu/unju/fi/service/imp/EmpleadoServiceMysqlImp.java
@@ -0,0 +1,45 @@
+package ar.edu.unju.fi.service.imp;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import ar.edu.unju.fi.entity.Empleado;
+import ar.edu.unju.fi.repository.IEmpleadoRepository;
+import ar.edu.unju.fi.service.IEmpleadoService;
+
+public class EmpleadoServiceMysqlImp implements IEmpleadoService {
+	
+	@Autowired
+	private IEmpleadoRepository empleadoRepository;
+	
+	@Autowired
+	private Empleado empleado;
+
+	@Override
+	public List<Empleado> getEmpleados() {
+		return empleadoRepository.findByEstado("activo");
+	}
+
+	@Override
+	public Empleado getEmpleado() {
+		return empleado;
+	}
+
+	@Override
+	public void guardar(Empleado empleado) {
+		empleadoRepository.save(empleado);
+	}
+
+	@Override
+	public void eliminar(Empleado empleado) {
+		empleado.setEstado("deBaja");
+		empleadoRepository.save(empleado);
+	}
+
+	@Override
+	public void recuperar(Long id) {
+		empleadoRepository.findById(id);
+	}
+
+}
